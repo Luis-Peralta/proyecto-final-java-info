063ddbfdf8cb6029bb3e0e8085cea4bb4b8aa322
Luis Peralta <luis_peralta_000@hotmail.com>
2020-12-26 17:59:28 -0300

    se agrega collection postman de cada entidad

--- Query-SQL.sql
+++ Query-SQL.sql
@@ -1,5 +1,15 @@
-#drop table comentarios,posts,posts_comment,usuarios,usuarios_post;
+select * from comentarios;
 
+select * from usuarios;
 
+select * from posts;
 
+select * from comentarios where post=4;
 
+delete from comentarios where id=9;
+
+select * from usuarios;
+
+select * from comentarios c where post=2;
+
+select * from comentarios c where post=2 ORDER BY c.id desc LIMIT 3;
